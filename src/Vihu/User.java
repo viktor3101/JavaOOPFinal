8a9cc8232a65046ba40e4b6fd3d273b798eede19
Viktor Husakivskiy <viktorgusakivskii@gmail.com>
2018-10-10 14:56:23 +0300

    add *.java

---
+++ src/Vihu/User.java
@@ -0,0 +1,14 @@
+package Vihu;
+
+public class User {
+    public final long id;
+    public final String username;
+    public final String password;
+
+    public User(long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+}
---
+++ src/Vihu/UsersArray.java
@@ -0,0 +1,50 @@
+package Vihu;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UsersArray {
+    public static ArrayList<User> users = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+    private static long id = 0;
+
+    public static void addUser() {
+        System.out.print("Input username: ");
+        String tempUN = scanner.next();
+        System.out.print("Input password: ");
+        String tempPass = scanner.next();
+        User user = new User(id, tempUN, tempPass);
+        id++;
+        users.add(user);
+    }
+
+    public static void deleteUser() {
+        System.out.print("Input id of user, you wanna delete: ");
+        long tempid = scanner.nextLong();
+        User tempUser = null;
+        for (User user : users) {
+            if (user.id == tempid) {
+                tempUser = user;
+                break;
+            }
+        }
+        System.out.print("Input your password: ");
+        String pass = scanner.next();
+        if (pass.equals(tempUser.password)) {
+            System.out.println("your account will be deleted:)");
+            users.remove(tempUser);
+        }
+    }
+
+    public static void changeUser() {
+
+    }
+
+    public static void loginUser() {
+
+    }
+
+    public static void logoutUser() {
+
+    }
+}
