f97fafe70bc17ddfa686521da5204a8f0347785f
Viktor Husakivskiy <viktorgusakivskii@gmail.com>
2018-10-10 23:35:20 +0300

    Final add *.java

--- src/Vihu/NotesRunner.java
+++ src/Vihu/NotesRunner.java
@@ -30,7 +30,7 @@ public class NotesRunner {
         user2.arrayDateSearch();
 
         User user3 = users.loginUser();//Bob`s notes:)
-        user3.arrayAddNote("feel HAPPY! my Katy is wife now","the happiest",LocalDateTime.now());
+        user3.arrayAddNote("feel HAPPY! my Katy is wife now","the happiest dad",LocalDateTime.now());
         System.out.println("Your id is: " + user3.seeMyId());
 
 
@@ -44,3 +44,64 @@ public class NotesRunner {
         System.out.println(users.showUser());
     }
 }
+/**
+ * Paul
+ * input password:
+ * pas1
+ * What will you wanna change?? (username/password)
+ * username
+ * Paul1997
+ * What will you wanna change?? (username/password)
+ * password
+ * 12345
+ * Notes:
+ * | 2018-10-10T23:31:32.937435
+ * | i am married
+ * | feelings: the best
+ * | 2018-10-09T23:31:32.937419
+ * | I am stupid
+ * | feelings: crazy
+ * | 2018-10-08T23:31:32.937387
+ * | No comments
+ * | feelings: very sad
+ * | 2018-10-05T23:31:32.937344
+ * | Feeling bad
+ * | feelings: sad
+ *
+ * input username:
+ * Katy
+ * input password:
+ * pas2
+ * Notes:
+ * | 2018-10-10T23:32:28.173018
+ * | i am married
+ * | feelings: the best
+ * | 2018-10-03T23:32:28.172945
+ * | he really wanna me be his wife
+ * | feelings: crazy
+ * | 2018-09-10T23:32:28.172892
+ * | he will marry me
+ * | feelings: beautiful
+ *
+ * input username:
+ * Bob
+ * input password:
+ * pas3
+ * Your id is: 3
+ * Input username: Viktor
+ * Input password: 123
+ * input username:
+ * Viktor
+ * input password:
+ * 123
+ * input your note:
+ * MyNote:)
+ * input your feelings:
+ * well
+ * 4
+ * [Paul1997, Katy, Bob, Viktor]
+ * input password:
+ * 123
+ * your account will be deleted:)
+ * [Paul1997, Katy, Bob]
+ * **/
